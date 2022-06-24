dd9e2efa762ff8324c09250a38f2b45a236b6641
Morikiyo Inoha <morikiyo@sonicgarden.jp>
2022-06-24 15:48:49 +0900

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-echo 'in WORKSPACE'
+set -eux
+
+echo 'in GITHUB_WORKSPACE'
+echo $GITHUB_WORKSPACE
 ls $GITHUB_WORKSPACE
+
+echo 'in GITHUB_ACTION_PATH'
+echo $GITHUB_ACTION_PATH
+ls $GITHUB_ACTION_PATH
 
 echo 'which?'
 which ruby
